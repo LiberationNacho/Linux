b99db54f018dc280e257e9f680c66d88a2148300
henac <man25816@naver.com>
2024-04-15 13:07:25 +0900

    주석 추가 1

--- server.c
+++ server.c
@@ -28,14 +28,19 @@ void *handle_client(void * arg){
 	while(1){
 		// 클라이언트가 보낸 메시지 받기
 		memset(buf, 0, sizeof(buf));
+		
+		// 메시지를 받는 것을 실패 한 경우
 		if(read(cSockfd, buf, sizeof(buf)) == -1){
-			perror("read");
+			// 시스템 오류 메시지를 출력하는 함수
+			perror("read"); // read: Connection reset by peer
 			close(cSockfd);
-			pthread_exit(NULL);
+			pthread_exit(NULL); // 스레드 종료
 		}
+
+		// 받은 베시지 출력
 		printf("Received from client: %s", buf);
 
-		//받은 메시지를 다시 클라이언트에게 보내기
+		//받은 메시지를 다시 클라이언트에게 보내기(답장)
 		printf("Input Response: ");
 		fgets(buf, sizeof(buf), stdin);
 		write(cSockfd, buf, strlen(buf));
@@ -48,7 +53,13 @@ void *handle_client(void * arg){
 int main(int argc, char *argv[]){
 	int sockfd, cSockfd, port;
 	struct sockaddr_in servaddr, cliaddr;
+
+	// typedef int socklen_t;
+	//소켓 API함수에서 주소 구조체의 크기를 전달하는데 사용되는 데이터 타입
 	socklen_t len;
+
+	//  POSIX스레드를 나타내는 데이터 형식, 각 스레드의 고유한 식별자
+	// fork()함수의 pid와 비슷한 듯
 	pthread_t tid[MAX_CLIENTS];
 	int i = 0;
 
@@ -77,12 +88,24 @@ int main(int argc, char *argv[]){
 		perror("bind");
 		return 1;
 	}
+	/*
+	   bind(): 소켓에 주소를 할당하는 역할
+	   소켓을 로컬 주소와 포트에 바인딩하여 해당 주소와 포트로 들어오는 연결을 수락하거나 데이터를 받을 수 있도록 준비한다.
+	   sockfd: 바인딩할 소켓의 파일 디스크립터입니다. socket() 함수로 생성한 소켓의 파일 디스크립터를 전달합니다.
+	   addr: 바인딩할 주소 정보를 담고 있는 struct sockaddr 구조체의 포인터입니다. 보통 struct sockaddr_in 구조체를 사용합니다.
+	   addrlen: addr 구조체의 크기를 나타내는 값입니다.
+	 */
 
 	// 연결 요청 대기열 설정
 	if (listen(sockfd, MAX_CLIENTS) == -1){
 		perror("listen");
 		return 1;
 	}
+	/*
+	   listen(): 소켓이 들어오는 연결을 수신할 수 있도록 설정하는 역할
+	   sockfd: 연결을 대기할 소켓의 파일 디스크립터입니다. socket() 함수로 생성한 소켓의 파일 디스크립터를 전달합니다.
+	   backlog: 연결 요청 대기 큐의 최대 길이를 지정합니다. 이는 동시에 처리 가능한 최대 연결 요청 수를 결정합니다.
+	*/
 
 	// 클라이언트 연결 수락 및 통신
 	while(1){
@@ -93,11 +116,20 @@ int main(int argc, char *argv[]){
 			return 1;
 		}
 
+		// 새로운 스레드를 생성하여 handle_client함수 실행
 		if (pthread_create(&tid[i++], NULL, handle_client, &cSockfd) != 0){
 			perror("pthread_create");
 			return 1;
 		}
+		/*
+		   thread: 새로 생성된 스레드의 식별자를 저장할 변수의 포인터입니다.
+		   attr: 스레드의 속성을 지정하는데 사용되는 옵션입니다. 보통 NULL로 설정하여 기본 속성을 사용합니다.
+		   start_routine: 새로운 스레드가 실행할 함수의 포인터입니다.
+		   arg: start_routine 함수에 전달될 인자입니다.
+		*/
 
+
+		// 스레드와 클라이언트 관리
 		if(i >= MAX_CLIENTS){
 			i = 0;
 			while(i < MAX_CLIENTS){
