b1525ef40f82dca14d814c75a2eaa641ab190423
man25816@stu.kmu.ac.kr <man25816@stu.kmu.ac.kr>
2024-05-04 13:37:42 +0900

    시그널 추가

--- sig/reciver.cpp
+++ sig/reciver.cpp
@@ -5,14 +5,31 @@
 
 using namespace std;
 
+// SIGINT
 void signalHandler_intr(int signum) {
     cout << "Received signal: " << signum << endl;
 	cout << "intr" << endl;
 	exit(signum);
 }
 
+// SIGTERM 
+void signalHandler_term(int signum) {
+    cout << "Received signal: " << signum << endl;
+	cout << "term" << endl;
+	exit(signum);
+}
+
+// SIGUSR
+void signalHandler_term(int signum) {
+    cout << "Received signal: " << signum << endl;
+	cout << "usr" << endl;
+    exit();
+}
+
 int main() {
-    signal(SIGINT, signalHandler_intr); // 시그널 핸들러 등록
+    signal(SIGINT, signalHandler_intr); // 시그널 핸들러 등록(인터럽트)
+    signal(SIGTERM, signalHandler_term); // 시그널 핸들러 등록(터미널)
+    signal(10, signalHandler_usr); // 시그널 핸들러 등록(터미널)
 
     cout << "Process ID: " << getpid() << endl;
 
