085ac00c763725b8bb842a3911139d8dab246918
man25816@stu.kmu.ac.kr <man25816@stu.kmu.ac.kr>
2024-05-26 12:09:36 +0900

    코드 수정 밑 다이어그렘 추가

--- sync/rwlock_mutex.c
+++ sync/rwlock_mutex.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/time.h>
+#include <limits.h>
 
 // 공유 자원
 #define ARRAY_SIZE 100
@@ -68,33 +69,64 @@ void rwlock_release_write_lock(RWLock *rwlock) {
 
 // 테스트 코드
 RWLock rwlock;
+long read_times[20];
+long write_times[5];
 
 // 읽기 스레드 함수
 void* reader(void* arg) {
+    struct timeval start, end;
+    gettimeofday(&start, NULL);
+
     rwlock_acquire_read_lock(&rwlock);
     printf("Reader %ld acquired the read lock\n", (long)arg);
-    // 공유 자원을 읽는 작업 수행 (임의의 시간 동안)
-    int sum = 0;
+
+    // 공유 자원을 읽는 작업 수행 (복잡한 작업)
+    int sum = 0, max = INT_MIN;
+    double average = 0.0;
     for (int i = 0; i < ARRAY_SIZE; i++) {
         sum += shared_array[i];
+        if (shared_array[i] > max) {
+            max = shared_array[i];
+        }
     }
-    usleep((rand() % 91 + 10) * 1000); // 10 ~ 100 ms 대기
+    average = (double)sum / ARRAY_SIZE;
+
     rwlock_release_read_lock(&rwlock);
-    printf("Reader %ld released the read lock\n", (long)arg);
+    printf("Reader %ld released the read lock (sum: %d, max: %d, avg: %.2f)\n", (long)arg, sum, max, average);
+
+    gettimeofday(&end, NULL);
+    long seconds = end.tv_sec - start.tv_sec;
+    long micros = ((seconds * 1000000) + end.tv_usec) - start.tv_usec;
+    read_times[(long)arg] = micros;
     return NULL;
 }
 
 // 쓰기 스레드 함수
 void* writer(void* arg) {
+    struct timeval start, end;
+    gettimeofday(&start, NULL);
+
     rwlock_acquire_write_lock(&rwlock);
     printf("Writer %ld acquired the write lock\n", (long)arg);
-    // 공유 자원을 쓰는 작업 수행 (임의의 시간 동안)
+
+    // 공유 자원을 쓰는 작업 수행 (복잡한 작업)
     for (int i = 0; i < ARRAY_SIZE; i++) {
         shared_array[i] = rand() % 100;
     }
-    usleep((rand() % 91 + 10) * 1000); // 10 ~ 100 ms 대기
+
+    // 수정 후 검증 작업
+    int check_sum = 0;
+    for (int i = 0; i < ARRAY_SIZE; i++) {
+        check_sum += shared_array[i];
+    }
+
     rwlock_release_write_lock(&rwlock);
-    printf("Writer %ld released the write lock\n", (long)arg);
+    printf("Writer %ld released the write lock (check_sum: %d)\n", (long)arg, check_sum);
+
+    gettimeofday(&end, NULL);
+    long seconds = end.tv_sec - start.tv_sec;
+    long micros = ((seconds * 1000000) + end.tv_usec) - start.tv_usec;
+    write_times[(long)arg] = micros;
     return NULL;
 }
 
@@ -136,6 +168,18 @@ int main() {
     long micros = ((seconds * 1000000) + end.tv_usec) - start.tv_usec;
     printf("Execution time: %ld seconds and %ld microseconds\n", seconds, micros);
 
+    // 개별 스레드 시간 계산 및 출력
+    long total_read_time = 0, total_write_time = 0;
+    for (int i = 0; i < 20; i++) {
+        total_read_time += read_times[i];
+    }
+    for (int i = 0; i < 5; i++) {
+        total_write_time += write_times[i];
+    }
+    printf("Total read time: %ld microseconds\n", total_read_time);
+    printf("Total write time: %ld microseconds\n", total_write_time);
+    printf("Average read time: %ld microseconds\n", total_read_time / 20);
+    printf("Average write time: %ld microseconds\n", total_write_time / 5);
+
     return 0;
 }
-
