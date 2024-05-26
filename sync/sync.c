32c6d22af0f1d20a87754ba931a0c9c6ae5c3ccc
man25816@stu.kmu.ac.kr <man25816@stu.kmu.ac.kr>
2024-05-26 17:37:40 +0900

    .

--- sync/sync.c
+++ sync/sync.c
@@ -35,7 +35,7 @@ void print_rankings() {
     printf("\n");
 }
 
-double read_rankings_rwlock(void* arg) {
+void* read_rankings_rwlock(void* arg) {
     struct timespec start, end;
     clock_gettime(CLOCK_MONOTONIC, &start);
     pthread_rwlock_rdlock(&rwlock);
@@ -43,10 +43,10 @@ double read_rankings_rwlock(void* arg) {
     pthread_rwlock_unlock(&rwlock);
     clock_gettime(CLOCK_MONOTONIC, &end);
     double elapsed = get_time_diff(start, end);
-    return elapsed;
+    return (void*)elapsed;
 }
 
-double write_rankings_rwlock(void* arg) {
+void* write_rankings_rwlock(void* arg) {
     struct timespec start, end;
     clock_gettime(CLOCK_MONOTONIC, &start);
     pthread_rwlock_wrlock(&rwlock);
@@ -56,10 +56,10 @@ double write_rankings_rwlock(void* arg) {
     pthread_rwlock_unlock(&rwlock);
     clock_gettime(CLOCK_MONOTONIC, &end);
     double elapsed = get_time_diff(start, end);
-    return elapsed;
+    return (void*)elapsed;
 }
 
-double read_rankings_mutex(void* arg) {
+void* read_rankings_mutex(void* arg) {
     struct timespec start, end;
     clock_gettime(CLOCK_MONOTONIC, &start);
     pthread_mutex_lock(&mutex);
@@ -67,10 +67,10 @@ double read_rankings_mutex(void* arg) {
     pthread_mutex_unlock(&mutex);
     clock_gettime(CLOCK_MONOTONIC, &end);
     double elapsed = get_time_diff(start, end);
-    return elapsed;
+    return (void*)elapsed;
 }
 
-double write_rankings_mutex(void* arg) {
+void* write_rankings_mutex(void* arg) {
     struct timespec start, end;
     clock_gettime(CLOCK_MONOTONIC, &start);
     pthread_mutex_lock(&mutex);
@@ -80,7 +80,7 @@ double write_rankings_mutex(void* arg) {
     pthread_mutex_unlock(&mutex);
     clock_gettime(CLOCK_MONOTONIC, &end);
     double elapsed = get_time_diff(start, end);
-    return elapsed;
+    return (void*)elapsed;
 }
 
 int main() {
@@ -99,7 +99,9 @@ int main() {
 
     printf("RWLock:\n");
     for (int i = 0; i < iterations; i++) {
-        double* elapsed_times[10];
+        // 이중 포인터를 사용하지 않고 단일 포인터로 변경
+        double elapsed_times[10]; // 수정된 부분
+
         for (int j = 0; j < 5; j++) {
             pthread_create(&threads[j], NULL, read_rankings_rwlock, NULL);
         }
@@ -107,13 +109,14 @@ int main() {
             pthread_create(&threads[j], NULL, write_rankings_rwlock, NULL);
         }
         for (int j = 0; j < 10; j++) {
+            // 반환값 받을 변수 주소를 &elapsed_times[j]로 변경
             pthread_join(threads[j], (void**)&elapsed_times[j]);
         }
         for (int j = 0; j < 5; j++) {
-            total_rwlock_read_time += *elapsed_times[j];
+            total_rwlock_read_time += elapsed_times[j];
         }
         for (int j = 5; j < 10; j++) {
-            total_rwlock_write_time += *elapsed_times[j];
+            total_rwlock_write_time += elapsed_times[j];
         }
     }
 
@@ -122,8 +125,8 @@ int main() {
 
     printf("\nMutex:\n");
     for (int i = 0; i < iterations; i++) {
+        double elapsed_times[10]; // 수정된 부분
 
-        double* elapsed_times[10];
         for (int j = 0; j < 5; j++) {
             pthread_create(&threads[j], NULL, read_rankings_mutex, NULL);
         }
@@ -134,10 +137,10 @@ int main() {
             pthread_join(threads[j], (void**)&elapsed_times[j]);
         }
         for (int j = 0; j < 5; j++) {
-            total_mutex_read_time += *elapsed_times[j];
+            total_mutex_read_time += elapsed_times[j];
         }
         for (int j = 5; j < 10; j++) {
-            total_mutex_write_time += *elapsed_times[j];
+            total_mutex_write_time += elapsed_times[j];
         }
     }
 
