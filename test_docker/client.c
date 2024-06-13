186c9a824a5f1467ced1e6dc860f2c660c64e8b2
henac <man25816@naver.com>
2024-06-13 20:30:22 +0900

    서버 클라이언트 스레드 풀, 뮤텍스 추가

--- test_docker/client.c
+++ test_docker/client.c
@@ -14,6 +14,7 @@ int client_socket; // 클라이언트 소켓
 void *receive_handler(void *socket_desc); // 메시지 수신 처리 함수
 void signal_handler(int sig); // 시그널 핸들러 함수
 char name[NAME_SIZE]; // 클라이언트 이름
+volatile sig_atomic_t flag = 0; // 서버 연결 끊김 플래그
 
 int main() {
     struct sockaddr_in server_addr;
@@ -24,12 +25,21 @@ int main() {
 
     // 클라이언트 소켓 생성
     client_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (client_socket == -1) {
+        perror("Could not create socket");
+        return 1;
+    }
+
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
     server_addr.sin_port = htons(PORT);
 
     // 서버에 연결
-    connect(client_socket, (struct sockaddr*)&server_addr, sizeof(server_addr));
+    if (connect(client_socket, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
+        perror("Connect failed");
+        close(client_socket);
+        return 1;
+    }
 
     // 클라이언트 이름 입력 및 서버에 전송
     printf("Enter your name: ");
@@ -38,15 +48,27 @@ int main() {
     send(client_socket, name, NAME_SIZE, 0);
 
     // 메시지 수신 처리 스레드 생성
-    pthread_create(&receive_thread, NULL, receive_handler, (void*)&client_socket);
+    if (pthread_create(&receive_thread, NULL, receive_handler, (void*)&client_socket) != 0) {
+        perror("Could not create thread");
+        close(client_socket);
+        return 1;
+    }
 
     char message[BUFFER_SIZE];
     while (1) {
+        if (flag) break; // 서버와의 연결이 끊어지면 루프 종료
+
         fgets(message, BUFFER_SIZE, stdin);
-        send(client_socket, message, strlen(message), 0); // 메시지 전송
+        message[strcspn(message, "\n")] = 0; // 개행 문자 제거
+
+        if (strlen(message) > 0) {
+            send(client_socket, message, strlen(message), 0); // 메시지 전송
+        }
     }
 
+    // 자원 정리
     close(client_socket);
+    pthread_join(receive_thread, NULL);
     return 0;
 }
 
@@ -57,9 +79,15 @@ void *receive_handler(void *socket_desc) {
     int read_size;
     while ((read_size = recv(sock, buffer, BUFFER_SIZE, 0)) > 0) {
         buffer[read_size] = '\0';
-        printf("%s", buffer); // 수신한 메시지 출력
+        printf("%s\n", buffer); // 수신한 메시지 출력
     }
-    return 0;
+
+    if (read_size == 0 || read_size == -1) {
+        printf("Disconnected from server.\n");
+        flag = 1; // 연결 끊김 플래그 설정
+    }
+
+    return NULL;
 }
 
 // 클라이언트 종료 처리 함수
--- test_docker/server.c
+++ test_docker/server.c
@@ -20,7 +20,7 @@ typedef struct {
 client_t *clients[MAX_CLIENTS]; // 클라이언트 리스트
 pthread_mutex_t clients_mutex = PTHREAD_MUTEX_INITIALIZER; // 클라이언트 리스트 보호용 뮤텍스
 
-void handle_client(client_t *client); // 클라이언트 처리 함수
+void *handle_client(void *arg); // 클라이언트 처리 함수 (스레드 함수)
 void send_message(char *message, client_t *exclude_client); // 메시지 전송 함수
 void signal_handler(int sig); // 시그널 핸들러 함수
 
@@ -34,19 +34,38 @@ int main() {
 
     // 서버 소켓 생성
     server_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_socket == -1) {
+        perror("Could not create socket");
+        return 1;
+    }
+
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = INADDR_ANY;
     server_addr.sin_port = htons(PORT);
 
-    // 소켓 바인드 및 리스닝 설정
-    bind(server_socket, (struct sockaddr*)&server_addr, sizeof(server_addr));
-    listen(server_socket, 10);
+    // 소켓 바인드
+    if (bind(server_socket, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
+        perror("Bind failed");
+        close(server_socket);
+        return 1;
+    }
+
+    // 리스닝 설정
+    if (listen(server_socket, 10) < 0) {
+        perror("Listen failed");
+        close(server_socket);
+        return 1;
+    }
 
     printf("Chat server started on port %d\n", PORT);
 
     // 클라이언트 연결 대기
     while (1) {
         client_socket = accept(server_socket, (struct sockaddr*)&client_addr, &client_addr_len);
+        if (client_socket < 0) {
+            perror("Accept failed");
+            continue;
+        }
 
         pthread_mutex_lock(&clients_mutex);
         for (int i = 0; i < MAX_CLIENTS; ++i) {
@@ -57,31 +76,41 @@ int main() {
                 recv(client_socket, new_client->name, NAME_SIZE, 0); // 클라이언트 이름 수신
                 clients[i] = new_client;
                 pthread_t tid;
-                pthread_create(&tid, NULL, (void*)handle_client, (void*)new_client);
+                pthread_create(&tid, NULL, handle_client, (void*)new_client);
                 break;
             }
         }
         pthread_mutex_unlock(&clients_mutex);
     }
 
+    close(server_socket);
     return 0;
 }
 
-// 클라이언트 처리 함수
-void handle_client(client_t *client) {
+// 클라이언트 처리 함수 (스레드 함수)
+void *handle_client(void *arg) {
+    client_t *client = (client_t*)arg;
     char buffer[BUFFER_SIZE];
     int read_size;
-    
+
     // 연결된 클라이언트의 참여 알림
-    sprintf(buffer, "%s has joined\n", client->name);
+    snprintf(buffer, sizeof(buffer), "%s has joined\n", client->name);
+    printf("%s", buffer); // 서버 콘솔에 출력
     send_message(buffer, client);
 
     // 클라이언트로부터 메시지 수신 및 전송
-    while ((read_size = recv(client->socket, buffer, BUFFER_SIZE, 0)) > 0) {
+    while ((read_size = recv(client->socket, buffer, BUFFER_SIZE - 1, 0)) > 0) {
         buffer[read_size] = '\0';
-        char message[BUFFER_SIZE + NAME_SIZE];
-        sprintf(message, "%s: %s", client->name, buffer); // 클라이언트 이름 포함한 메시지 작성
-        send_message(message, client);
+
+        // 메시지 작성
+        size_t message_len = strlen(client->name) + 2 + strlen(buffer) + 1;
+        char *message = (char*)malloc(message_len);
+        if (message != NULL) {
+            snprintf(message, message_len, "%s: %s", client->name, buffer);
+            printf("%s", message); // 서버 콘솔에 출력
+            send_message(message, client);
+            free(message);
+        }
     }
 
     // 클라이언트 연결 종료 처리
@@ -95,11 +124,13 @@ void handle_client(client_t *client) {
         }
     }
     pthread_mutex_unlock(&clients_mutex);
-    
-    sprintf(buffer, "%s has left\n", client->name);
+
+    snprintf(buffer, sizeof(buffer), "%s has left\n", client->name);
+    printf("%s", buffer); // 서버 콘솔에 출력
     send_message(buffer, client);
 
     free(client);
+    return NULL;
 }
 
 // 메시지 전송 함수
